d4b24bb475369a5537bce10ae032f3a5b8bc4b44
Ubuntu <ubuntu@ip-172-31-72-207.ec2.internal>
2020-07-23 02:59:43 +0000

    invalid schedule?

--- TACO_Benchmarks/gausspyramid/gausspyramid_generator.cpp
+++ TACO_Benchmarks/gausspyramid/gausspyramid_generator.cpp
@@ -13,7 +13,7 @@ using namespace Halide;
 //const int blockSize = 3;
 //int imgSize = 1024 / 2 - 1;
 
-const int pyramid_levels = 4;
+const int pyramid_levels = 6;
 
 class GaussianBlur : public Halide::Generator<GaussianBlur> {
 public:
@@ -23,8 +23,13 @@ public:
     Var x, y, c;
 
     Func downsample(Func f) {
-      f(x, y) = f(2*x, 2*y);
-      return f;
+      RDom reduce(-1, 1, -1, 1);
+      Func ds;
+      ds(x, y) = 0;
+      ds(x, y) += f(x + reduce.x, y + reduce.y);
+      Func avg;
+      avg(x, y) = ds(x, y) / Expr(9);
+      return avg;
     }
 
     void generate() {
@@ -38,20 +43,20 @@ public:
         input_copy(x, y) = cast<uint16_t>(clamped(x, y));
         hw_input(x, y) = input_copy(x, y);
 
-        //Func gPyramid[pyramid_levels];
-        //gPyramid[0](x, y) =
-          //hw_input(x, y);
+        Func gPyramid[pyramid_levels];
+        gPyramid[0](x, y) =
+          hw_input(x, y);
 
-        //for (int j = 1; j < pyramid_levels; j++) {
-          //gPyramid[j](x, y) =
-            //downsample(gPyramid[j - 1])(x, y);
-        //}
+        for (int j = 1; j < pyramid_levels; j++) {
+          gPyramid[j](x, y) =
+            downsample(gPyramid[j - 1])(x, y);
+        }
 
         Func hw_output;
         hw_output(x, y) =
-          cast<uint16_t>( hw_input(x, y) );
+          //cast<uint16_t>( hw_input(x, y) );
           //cast<uint16_t>( gPyramid[0](x, y) );
-          //cast<uint16_t>( gPyramid[pyramid_levels - 1](x, y) );
+          cast<uint16_t>( gPyramid[pyramid_levels - 1](x, y) );
         output(x, y) = hw_output(x, y);
 
         input.dim(0).set_bounds_estimate(0, 2048*2);
