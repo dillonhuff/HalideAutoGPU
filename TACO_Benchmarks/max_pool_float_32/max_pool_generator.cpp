8c06ba91f9e66ab022a1eca4f75c7da133878539
Ubuntu <ubuntu@ip-172-31-72-207.ec2.internal>
2021-01-09 04:19:05 +0000

    Adding separate max pool fp

--- TACO_Benchmarks/max_pool_float_32/max_pool_generator.cpp
+++ TACO_Benchmarks/max_pool_float_32/max_pool_generator.cpp
@@ -18,26 +18,10 @@ const int pyramid_levels = 4;
 
 class GaussianBlur : public Halide::Generator<GaussianBlur> {
 public:
-    Input<Buffer<uint16_t>>  input{"input", 3};
-    Output<Buffer<uint16_t>> output{"output", 3};
+    Input<Buffer<float>>  input{"input", 3};
+    Output<Buffer<float>> output{"output", 3};
 
     Var x, y, c;
-
-    Func downsample(Func f) {
-      //Func ds;
-      //ds(x, y) = (3*f(2*x + 1, 2*y) + f(2*x, 2*y));
-      //return ds;
-
-      RDom reduce(-1, 1, -1, 1);
-
-      Func ds;
-      ds(x, y) = 0.0f;
-      ds(x, y) += f(2*x + reduce.x, 2*y + reduce.y);
-      Func avg;
-      //avg(x, y) = ds(x, y) / Expr(9);
-      avg(x, y) = ds(x, y) / Expr(9.0f);
-      return avg;
-    }
 
     void generate() {
         /* THE ALGORITHM */
@@ -48,31 +32,26 @@ public:
         Func clamped = Halide::BoundaryConditions::repeat_edge(input);
 
         Func hw_input, input_copy;
-        input_copy(x, y, c) = cast<uint16_t>(clamped(x, y, c));
+        input_copy(x, y, c) = cast<float>(clamped(x, y, c));
         hw_input(x, y, c) = input_copy(x, y, c);
 
-        //RDom dom(0, 2, 0, 2);
         Func hw_output;
         hw_output(x, y, c) =
-          cast<uint16_t>(max(
+          cast<float>(max(
                 hw_input(2*x, 2*y, c),
                 hw_input(2*x + 1, 2*y, c),
                 hw_input(2*x, 2*y + 1, c),
                 hw_input(2*x + 1, 2*y + 1, c)));
 
-          //cast<uint16_t>( hw_input(x * 2, y * 2, c) );
         output(x, y, c) = hw_output(x, y, c);
 
         input.dim(0).set_bounds_estimate(0, 2048*2);
         input.dim(1).set_bounds_estimate(0, 2048*2);
         input.dim(2).set_bounds_estimate(0, 2048*2);
 
-        output.estimate(x, 0, 2048)
-              .estimate(y, 0, 2048).
-              estimate(c, 0, 2048);
-
-        //output.bound(x, 0, 32);
-        //output.bound(y, 0, 32);
+        output.estimate(x, 0, 128)
+              .estimate(y, 0, 128).
+              estimate(c, 0, 64);
 
         if (auto_schedule) {
         } else {
