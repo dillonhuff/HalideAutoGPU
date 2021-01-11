c6ff9a1b11a9b5c26545a27c46b8c75d793f4853
Ubuntu <ubuntu@ip-172-31-48-185.ec2.internal>
2021-01-11 00:53:46 +0000

    Adding another mp variant

---
+++ TACO_Benchmarks/max_pool_float_32_compute_int_16_store/max_pool_generator.cpp
@@ -0,0 +1,69 @@
+/*
+ * An application for applying a Gaussian blur.
+ * It uses a 3x3 stencil with constant weights.
+ */
+
+#include "Halide.h"
+
+namespace {
+
+using namespace Halide;
+
+// Size of blur for gradients.
+//const int blockSize = 3;
+//int imgSize = 1024 / 2 - 1;
+
+const int pyramid_levels = 4;
+
+typedef uint16_t InPixelType;
+
+class GaussianBlur : public Halide::Generator<GaussianBlur> {
+public:
+    Input<Buffer<InPixelType>>  input{"input", 3};
+    Output<Buffer<InPixelType>> output{"output", 3};
+
+    Var x, y, c;
+
+    void generate() {
+        /* THE ALGORITHM */
+
+        Var xo("xo"), yo("yo"), xi("xi"), yi("yi");
+        //input.trace_loads();
+
+        Func clamped = Halide::BoundaryConditions::repeat_edge(input);
+
+        Func hw_input, input_copy;
+        input_copy(x, y, c) = cast<InPixelType>(clamped(x, y, c));
+        hw_input(x, y, c) = cast<float>(input_copy(x, y, c));
+
+        Func hw_output;
+        hw_output(x, y, c) =
+          cast<InPixelType>(max(
+                hw_input(2*x, 2*y, c),
+                hw_input(2*x + 1, 2*y, c),
+                hw_input(2*x, 2*y + 1, c),
+                hw_input(2*x + 1, 2*y + 1, c)));
+
+        output(x, y, c) = hw_output(x, y, c);
+
+        input.dim(0).set_bounds_estimate(0, 2048*2);
+        input.dim(1).set_bounds_estimate(0, 2048*2);
+        input.dim(2).set_bounds_estimate(0, 2048*2);
+
+        output.estimate(x, 0, 128)
+              .estimate(y, 0, 128).
+              estimate(c, 0, 64);
+
+        if (auto_schedule) {
+        } else {
+          //clamped.trace_loads();
+          clamped.compute_root();
+        }
+
+    }
+};
+
+}  // namespace
+
+HALIDE_REGISTER_GENERATOR(GaussianBlur, max_pool)
+
---
+++ TACO_Benchmarks/max_pool_float_32_compute_int_16_store/process.cpp
@@ -0,0 +1,93 @@
+#include <fstream>
+#include <cstdio>
+#include <chrono>
+// Hello this is a comment I am a comment
+// Yet another comment
+#include "max_pool.h"
+#ifndef NO_AUTO_SCHEDULE
+//#include "max_pool_auto_schedule_store.h"
+#include "max_pool_auto_schedule.h"
+#include "max_pool_cpu.h"
+#include "max_pool_simple_auto_schedule.h"
+#include "max_pool_auto_schedule_no_fus.h"
+#endif
+
+#include "benchmark_util.h"
+#include "HalideBuffer.h"
+#include "halide_image_io.h"
+
+#include <cmath>
+
+using namespace std;
+using namespace Halide::Runtime;
+using namespace Halide::Tools;
+using namespace std::chrono;
+
+int main(int argc, char **argv) {
+    if (argc < 7) {
+        printf("Usage: ./process input.png levels alpha beta timing_iterations output.png\n"
+               "e.g.: ./process input.png 8 1 1 10 output.png\n");
+        return 0;
+    }
+#ifdef cuda_alloc
+    halide_reuse_device_allocations(nullptr, true);
+#endif
+    // Input may be a PNG8
+    Buffer<uint16_t> input(128, 128, 64);
+    for (int c = 0; c < input.channels(); c++) {
+      for (int y = 0; y < input.height(); y++) {
+        for (int x = 0; x < input.width(); x++) {
+          input(x, y, c) = rand();
+        }
+      }
+    }
+
+    ofstream input_info("input_info.txt");
+    input_info << "x," << input.width() << endl;
+    input_info << "y," << input.height() << endl;
+    input_info << "b," << input.channels() << endl;
+    input_info.close();
+
+    Buffer<uint16_t> output(64, 64, 64);
+    max_pool_cpu(input, output);
+
+    const long int num_runs = 1000000;
+    //const long int num_runs = 1;
+    __int64_t start_us = duration_cast<microseconds>(system_clock::now().time_since_epoch()).count();
+
+    for (long int r = 0; r < num_runs; r++) {
+      //cout << "r = " << r << endl;
+      max_pool_auto_schedule(input, output); 
+      output.device_sync(); 
+    }
+    __int64_t end_us = duration_cast<microseconds>(system_clock::now().time_since_epoch()).count();
+
+    ofstream times("times.csv");
+    times << start_us << endl;
+    times << end_us << endl;
+    times << num_runs << endl;
+    times.close();
+    //for (int r = 0; r < 1; r++) {
+      //max_pool(input, output); 
+      //output.device_sync(); 
+    //}
+    cout << "Done with auto schedule" << endl;
+    //return 0;
+
+   multi_way_bench({
+        {"Manual", [&]() { max_pool(input, output); output.device_sync(); }},
+    #ifndef NO_AUTO_SCHEDULE
+        //{"Nested auto-scheduled", [&]() { max_pool_auto_schedule_store(input, output); output.device_sync(); }},
+       {"Auto-scheduled", [&]() { max_pool_auto_schedule(input, output); output.device_sync(); }},
+          {"No-fusion auto-scheduled", [&]() { max_pool_auto_schedule_no_fus(input, output); output.device_sync(); }},
+        {"Simple auto-scheduled", [&]() { max_pool_simple_auto_schedule(input, output); output.device_sync(); }}
+    #endif
+        }
+    );
+
+   std::cout << "Done, transferring image" << endl;
+
+    convert_and_save_image(output, argv[6]);
+
+    return 0;
+}
